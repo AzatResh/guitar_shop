9b92735ebaf9cd1a09643e3eb84103125ddfb748
Azat <115871245+AzatResh@users.noreply.github.com>
2023-05-22 16:26:57 +0600

    - плавный переход по якорным ссылкам

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,14 @@ window.addEventListener("DOMContentLoaded", () => {
         menu.classList.toggle('nav-menu_active');
     })
 })
+
+$("a[href*='#']").on('click', function() {
+    let href = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    }, {
+        duration: 370, // по умолчанию «400» 
+        easing: "linear" // по умолчанию «swing» 
+    });
+    return false;
+});
